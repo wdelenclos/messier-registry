546cacc073f06f27a85740fa47fb1c0ccab93f23
Wladimir Delenclos <wdelenclos@gmail.com>
2020-02-18 10:47:51 +0100

    (feat) Update board

--- index.py
+++ index.py
@@ -177,18 +177,55 @@ index = """
 </body>
 <script>
     function getAPIStatus(){
-
         if (window.fetch) {
-            fetch('http://localhost:5000/api/v1')
-                .then(response => {
-                    response.json()
-                        .then(console.log);
+            fetch('http://space.wdelenclos.fr/api/v1')
+                .then(
+                    function(response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                        return;
+                    }
+
+                    // Examine the text in the response
+                    response.json().then(function(data) {
+                        console.log(data.status);
+                    });
+                    }
+                )
+                .catch(function(err) {
+                    console.log('Fetch Error :-S', err);
                 });
         } else {
-        
+            
+        }
+    }
+    function getObj(){
+        if (window.fetch) {
+            fetch('http://space.wdelenclos.fr/api/v1/objects')
+                .then(
+                    function(response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                        return;
+                    }
+
+                    // Examine the text in the response
+                    response.json().then(function(data) {
+                        console.log(data.length);
+                    });
+                    }
+                )
+                .catch(function(err) {
+                    console.log('Fetch Error :-S', err);
+                });
+        } else {
+            
         }
     }
     getAPIStatus();
+    getObj();
 </script>
 </html>
 """
